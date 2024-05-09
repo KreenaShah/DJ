a276f18bb141775497215ec7577b387307c72f48
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2024-05-09 17:10:56 +0530

    aggregate amortized method -  binary counter

--- SEM6_Pracs/AA/codes/exp1/aggregateBinaryCounter.py
+++ SEM6_Pracs/AA/codes/exp1/aggregateBinaryCounter.py
@@ -1,36 +1,36 @@
 class BinaryCounter:
     def __init__(self):
-        self.counter = [0] * 4  # Initialize the counter with four zeros
-        self.total_cost = 0
-        self.noOfIncrements = 0
-        self.bits_flipped = 0
-
-    def increment(self):
-        self.bits_flipped = 0
-        i = 0
-        while i < len(self.counter) and self.counter[i] == 1:
-            self.counter[i] = 0
-            self.bits_flipped += 1
-            i += 1
-        if i < len(self.counter):
-            self.counter[i] = 1
-            self.bits_flipped += 1
-        self.noOfIncrements += 1
-        self.total_cost += self.bits_flipped
-
-    def cost(self):
-        return self.bits_flipped
+        self.counter = [0]*4
+        self.counterVal = 0
+        self.actCost = 0 # no. of bits flipped
+        self.totCost = 0 
+        self.noOfIncrements = 0 # no. of bits flipped
     
-    def amortized_cost(self):
-        return self.total_cost / self.noOfIncrements
+    def increment(self):
+        self.actCost = 0
+        i = 0
+        while(i<len(self.counter) and self.counter[i]==1):
+            self.counter[i] = 0
+            self.actCost += 1
+            i+=1
+        if(i<len(self.counter)):
+            self.counter[i] = 1
+            self.actCost += 1
+        self.noOfIncrements += 1
+        self.totCost += self.actCost
     
     def get_counter(self):
-        return self.counter[::-1]  # Reverse the counter before returning
+        return self.counter[::-1]
+
+    def actualCost(self):
+        return self.actCost
+    
+    def amortizedCost(self):
+        return self.totCost/self.noOfIncrements
 
 counter = BinaryCounter()
 print("CounterValue  binaryNumber    Cost")
 for i in range(1,9):
     counter.increment()
-    print(f"{i}             {counter.get_counter()}     {counter.cost()}")
-
-print("Amortized cost", counter.amortized_cost())
+    print(f"{i}             {counter.get_counter()}     {counter.actualCost()}")
+print("Amortized Cost : ",counter.amortizedCost())
