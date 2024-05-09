8fa81a17f5652384a4c92550fc274b896de737cd
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2024-05-09 17:28:47 +0530

    potential amortized method -  binary counter

--- SEM6_Pracs/AA/codes/exp1/potentialBinaryCounter.py
+++ SEM6_Pracs/AA/codes/exp1/potentialBinaryCounter.py
@@ -3,38 +3,42 @@ class BinaryCounter:
         self.counter = [0]*4
         self.counterVal = 0
         self.actCost = 0 # no. of bits flipped
-        self.amortCost = 0 # 2 for setting bit 1, 0 for setting bit 0
-        self.cred = 0
+        self.prevP = 0 
+        self.currP = 0 # no. of 1's in the current binary number
+        self.potentialDiff = 0 
+        self.amortCost = 0
     
     def increment(self):
         self.actCost = 0
-        self.amortCost = 0
+        self.currP = 0
         i = 0
         while(i<len(self.counter) and self.counter[i]==1):
             self.counter[i] = 0
             self.actCost += 1
-            self.amortCost += 0
             i+=1
         if(i<len(self.counter)):
             self.counter[i] = 1
             self.actCost += 1
-            self.amortCost += 2
-        self.cred += self.amortCost - self.actCost
+        for i in range(len(self.counter)):
+            if(self.counter[i]==1):
+                self.currP += 1
+        self.potentialDiff = self.currP - self.prevP
+        self.prevP = self.currP
     
     def get_counter(self):
         return self.counter[::-1]
-    
-    def amortizedCost(self):
-        return self.amortCost
 
     def actualCost(self):
         return self.actCost
     
-    def credit(self):
-        return self.cred
+    def potentailDifference(self):
+        return self.potentialDiff
+    
+    def amortizedCost(self):
+        return self.actCost + self.potentialDiff
 
 counter = BinaryCounter()
-print("CounterValue  binaryNumber    AmortizedCost    ActualCost      Credit")
+print("CounterValue  binaryNumber    ActualCost    PotentialDiff    AmortizedCost")
 for i in range(1,9):
     counter.increment()
-    print(f"{i}             {counter.get_counter()}         {counter.amortizedCost()}           {counter.actualCost()}              {counter.credit()}")
+    print(f"{i}             {counter.get_counter()}         {counter.actualCost()}         {counter.potentailDifference()}                 {counter.amortizedCost()}")
